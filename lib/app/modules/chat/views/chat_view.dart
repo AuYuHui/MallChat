1ee6ada5201c7c8a45dc2f2cb1bdeacf3a22f496
cyh <2419068625@qq.com>
2023-10-10 23:20:05 +0800

    feat: 使用 flutter_keyboard_visibility 点击后隐藏键盘

--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -3,6 +3,7 @@ import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:floor/floor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'package:get/get.dart';
@@ -33,180 +34,182 @@ class ChatView extends GetView<ChatController> {
         ),
         title: Text(title),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: EasyRefresh(
-              clipBehavior: Clip.none,
-              onLoad: () {
-                return chatController.getChatMessage();
-              },
-              header: ListenerHeader(
-                listenable: chatController.listenable,
-                triggerOffset: 100000,
-                clamping: false,
-              ),
-              footer: BuilderFooter(
-                  triggerOffset: 40,
+      body: KeyboardDismissOnTap(
+        child: Column(
+          children: [
+            Expanded(
+              child: EasyRefresh(
+                clipBehavior: Clip.none,
+                onLoad: () {
+                  return chatController.getChatMessage();
+                },
+                header: ListenerHeader(
+                  listenable: chatController.listenable,
+                  triggerOffset: 100000,
                   clamping: false,
-                  position: IndicatorPosition.above,
-                  infiniteOffset: null,
-                  processedDuration: Duration.zero,
-                  builder: (context, state) {
-                    return Stack(
-                      children: [
-                        SizedBox(
-                          height: state.offset,
-                          width: double.infinity,
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
-                          child: Container(
-                            alignment: Alignment.center,
+                ),
+                footer: BuilderFooter(
+                    triggerOffset: 40,
+                    clamping: false,
+                    position: IndicatorPosition.above,
+                    infiniteOffset: null,
+                    processedDuration: Duration.zero,
+                    builder: (context, state) {
+                      return Stack(
+                        children: [
+                          SizedBox(
+                            height: state.offset,
                             width: double.infinity,
-                            height: 40,
-                            child: const SpinKitCircle(
-                              size: 24,
-                              color: lightColor.tabBarSelectText,
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              alignment: Alignment.center,
+                              width: double.infinity,
+                              height: 40,
+                              child: const SpinKitCircle(
+                                size: 24,
+                                color: lightColor.tabBarSelectText,
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+                child: Obx(() => CustomScrollView(
+                      reverse: true,
+                      controller: chatController.scrollController,
+                      shrinkWrap: chatController.shrinkWrap.value,
+                      clipBehavior: Clip.none,
+                      slivers: [
+                        SliverPadding(
+                          padding: const EdgeInsets.all(15.0),
+                          sliver: SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                return MessageItem(
+                                  message: chatController.messages[index],
+                                );
+                              },
+                              childCount: chatController.messages.length,
                             ),
                           ),
-                        )
+                        ),
                       ],
-                    );
-                  }),
-              child: Obx(() => CustomScrollView(
-                    reverse: true,
-                    controller: chatController.scrollController,
-                    shrinkWrap: chatController.shrinkWrap.value,
-                    clipBehavior: Clip.none,
-                    slivers: [
-                      SliverPadding(
-                        padding: const EdgeInsets.all(15.0),
-                        sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              return MessageItem(
-                                message: chatController.messages[index],
-                              );
-                            },
-                            childCount: chatController.messages.length,
-                          ),
+                    )),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(minWidth: 55.0),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  CircularIcon(
+                    icon: Icons.sentiment_very_satisfied,
+                    onPressed: _showEmojiPicker,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: lightColor.tabBarBg, // 设置背景颜色
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(4),
+                          right: Radius.circular(4),
                         ),
                       ),
-                    ],
-                  )),
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.only(bottom: 10),
-            constraints: const BoxConstraints(minWidth: 55.0),
-            child: Row(
-              children: [
-                const SizedBox(
-                  width: 15,
-                ),
-                CircularIcon(
-                  icon: Icons.sentiment_very_satisfied,
-                  onPressed: _showEmojiPicker,
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: lightColor.tabBarBg, // 设置背景颜色
-                      borderRadius: BorderRadius.horizontal(
-                        left: Radius.circular(4),
-                        right: Radius.circular(4),
+                      constraints: const BoxConstraints(minHeight: 45),
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: TextField(
+                        onTap: () {
+                          if (chatController.focusNode.hasFocus) {
+                            chatController.emojiShow.value = true;
+                          }
+                        },
+                        controller: _textEditingController,
+                        textInputAction: TextInputAction.send, // 将发送按钮更改为 "发送"：
+                        maxLength: 200, // 最多 200个字
+                        maxLines: null, // 可以换行
+                        onSubmitted: _handleSubmitted,
+                        focusNode: chatController.focusNode,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none, counterText: ''),
                       ),
                     ),
-                    constraints: const BoxConstraints(minHeight: 45),
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: TextField(
-                      onTap: () {
-                        if (chatController.focusNode.hasFocus) {
-                          chatController.emojiShow.value = true;
-                        }
-                      },
-                      controller: _textEditingController,
-                      textInputAction: TextInputAction.send, // 将发送按钮更改为 "发送"：
-                      maxLength: 200, // 最多 200个字
-                      maxLines: null, // 可以换行
-                      onSubmitted: _handleSubmitted,
-                      focusNode: chatController.focusNode,
-                      decoration: const InputDecoration(
-                          border: InputBorder.none, counterText: ''),
-                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                CircularIcon(
-                  icon: Icons.keyboard_alt_outlined,
-                  onPressed: () {},
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                CircularIcon(
-                  icon: Icons.add,
-                  onPressed: () {},
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-              ],
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  CircularIcon(
+                    icon: Icons.keyboard_alt_outlined,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  CircularIcon(
+                    icon: Icons.add,
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Obx(() => Visibility(
-                visible: !chatController.emojiShow.value,
-                child: SizedBox(
-                  height: 270,
-                  child: EmojiPicker(
-                    textEditingController: _textEditingController,
-                    onBackspacePressed: _onBackspacePressed,
-                    config: Config(
-                      columns: 7,
-                      emojiSizeMax: 32 *
-                          (foundation.defaultTargetPlatform ==
-                                  TargetPlatform.iOS
-                              ? 1.30
-                              : 1.0),
-                      verticalSpacing: 0,
-                      horizontalSpacing: 0,
-                      gridPadding: EdgeInsets.zero,
-                      initCategory: Category.RECENT,
-                      bgColor: const Color(0xFFF2F2F2),
-                      indicatorColor: Colors.blue,
-                      iconColor: Colors.grey,
-                      iconColorSelected: Colors.blue,
-                      backspaceColor: Colors.blue,
-                      skinToneDialogBgColor: Colors.white,
-                      skinToneIndicatorColor: Colors.grey,
-                      enableSkinTones: true,
-                      recentTabBehavior: RecentTabBehavior.RECENT,
-                      recentsLimit: 28,
-                      replaceEmojiOnLimitExceed: false,
-                      noRecents: const Text(
-                        'No Recents',
-                        style: TextStyle(fontSize: 20, color: Colors.black26),
-                        textAlign: TextAlign.center,
+            Obx(() => Visibility(
+                  visible: !chatController.emojiShow.value,
+                  child: SizedBox(
+                    height: 270,
+                    child: EmojiPicker(
+                      textEditingController: _textEditingController,
+                      onBackspacePressed: _onBackspacePressed,
+                      config: Config(
+                        columns: 7,
+                        emojiSizeMax: 32 *
+                            (foundation.defaultTargetPlatform ==
+                                    TargetPlatform.iOS
+                                ? 1.30
+                                : 1.0),
+                        verticalSpacing: 0,
+                        horizontalSpacing: 0,
+                        gridPadding: EdgeInsets.zero,
+                        initCategory: Category.RECENT,
+                        bgColor: const Color(0xFFF2F2F2),
+                        indicatorColor: Colors.blue,
+                        iconColor: Colors.grey,
+                        iconColorSelected: Colors.blue,
+                        backspaceColor: Colors.blue,
+                        skinToneDialogBgColor: Colors.white,
+                        skinToneIndicatorColor: Colors.grey,
+                        enableSkinTones: true,
+                        recentTabBehavior: RecentTabBehavior.RECENT,
+                        recentsLimit: 28,
+                        replaceEmojiOnLimitExceed: false,
+                        noRecents: const Text(
+                          'No Recents',
+                          style: TextStyle(fontSize: 20, color: Colors.black26),
+                          textAlign: TextAlign.center,
+                        ),
+                        loadingIndicator: const SizedBox.shrink(),
+                        tabIndicatorAnimDuration: kTabScrollDuration,
+                        categoryIcons: const CategoryIcons(),
+                        buttonMode: ButtonMode.MATERIAL,
+                        checkPlatformCompatibility: true,
                       ),
-                      loadingIndicator: const SizedBox.shrink(),
-                      tabIndicatorAnimDuration: kTabScrollDuration,
-                      categoryIcons: const CategoryIcons(),
-                      buttonMode: ButtonMode.MATERIAL,
-                      checkPlatformCompatibility: true,
                     ),
                   ),
-                ),
-              ))
-        ],
+                ))
+          ],
+        ),
       ),
     );
   }
